71300fbbcb2f5a029ee41945b73c553dcbbfcea5
Jakub Kuś <jakub@jkus.dev>
2023-03-14 14:42:09 +0100

    add exercise 3.2, ticker

---
+++ 3-callbacks-and-events/3.2-ticker/index.js
@@ -0,0 +1,30 @@
+import { EventEmitter } from 'events';
+
+const ticker = (totalMs, cb) => {
+  const emitter = new EventEmitter();
+  let tickCount = 0, msPassed = 0;
+
+  if (totalMs < 50) {
+    process.nextTick(() => cb(0));
+    return emitter;
+  }
+
+  const handleTick = () => {
+    setTimeout(() => {
+      msPassed += 50;
+      tickCount += 1;
+      emitter.emit('tick', msPassed);
+      if (msPassed + 50 <= totalMs) {
+        handleTick();
+      } else {
+        cb(tickCount);
+      }
+    }, 50);
+  }
+  handleTick();
+
+  return emitter;
+}
+
+ticker(process.argv[2], tickCount => console.log(`total number of ticks: ${tickCount}`))
+  .on('tick', msPassed => console.log(`${msPassed}ms`));
