c7848ce185f75b90a5ab2a3688d61e6b5d6c61f5
Jakub Kuś <jakub@jkus.dev>
2024-10-26 17:47:30 +0200

    add exercise 4.2, list recursively

---
+++ 4-async-control-flow/4.2-list-recursively/index.js
@@ -0,0 +1,32 @@
+import fs from 'fs';
+import path from 'path';
+
+// HOW TO RUN: navigate to folder containing this file and run `node index.js` inside.
+// > cd 4-async-control-flow/4.2-list-recursively && node index.js
+
+const filesFound = [];
+let dirEnteredCount = 0;
+
+const listNestedFiles = (dir, cb) => {
+  dirEnteredCount++;
+
+  fs.readdir(dir, { withFileTypes: true }, (err, files) => {
+    if (err) return cb(err);
+
+    files.forEach(file => {
+      const filePath = path.join(dir, file.name);
+      if (file.isDirectory()) {
+        listNestedFiles(filePath, cb);
+      } else {
+        filesFound.push(filePath);
+      }
+    });
+
+    if (--dirEnteredCount === 0) cb(null, filesFound);
+  });
+};
+
+listNestedFiles('../', (err, files) => {
+  if (err) console.error(err.message);
+  else if (files) console.log({ files });
+});
