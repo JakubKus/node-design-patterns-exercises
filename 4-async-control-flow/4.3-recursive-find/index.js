520b33f5efdb000d85afb2a36f20e72a3d12cf18
Jakub Kuś <jakub@jkus.dev>
2024-10-27 18:20:13 +0100

    add exercise 4.3, recursive find

---
+++ 4-async-control-flow/4.3-recursive-find/index.js
@@ -0,0 +1,50 @@
+import fs from 'fs';
+import { listNestedFiles } from './list-recursively-copy.js';
+
+const ParallelFinder = params => {
+  const { files, keyword, cb } = params;
+
+  const CONCURRENCY = 4;
+
+  const searchedFilesFound = [];
+  let running = 0;
+  let tasksCompleted = 0;
+  let taskIdx = 0;
+
+  const tasks = files.map(file => {
+    return done => {
+      fs.readFile(file, 'utf8', (err, data) => {
+        if (err) return done(err);
+
+        const fileContent = data.toString();
+        if (fileContent.includes(keyword)) searchedFilesFound.push(file);
+        done();
+      });
+    };
+  });
+
+  const finish = () => cb(searchedFilesFound);
+
+  const next = () => {
+    while (running < CONCURRENCY && taskIdx < tasks.length) {
+      const task = tasks[taskIdx++];
+      task(() => {
+        if (++tasksCompleted === tasks.length) return finish();
+        running--;
+        next();
+      });
+      running++;
+    }
+  };
+
+  return { search: next };
+};
+
+const recursiveFind = (dir, keyword, cb) => {
+  listNestedFiles(dir, files => {
+    ParallelFinder({ files, keyword, cb }).search();
+  });
+};
+
+const keywordArg = process.argv[2];
+recursiveFind('./', keywordArg, console.log);
